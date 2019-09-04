71215b1ca7701c1dfcb5d31e8a283146a3f7b569
Krishna Vangala <currydeveloper@gmail.com>
2019-09-03 19:18:11 -0700

    Project Initiated.

---
+++ src/main/java/com/athena/bugtracker/BaseServlet.java
@@ -0,0 +1,23 @@
+package com.athena.bugtracker;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/BaseServlet")
+public class BaseServlet extends HttpServlet {
+    //Currently not in place
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World 123!</h3>");
+    }
+}
